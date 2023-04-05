b3314b3fa0afa0743ed51fe75f2dd2473464b1b8
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2023-04-06 00:11:16 +0500

    FEAT: Added more packages for UI construction.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  db: {
+    connection_string: "postgres://ShahzaibSE:LEv81lmCDogI@ep-holy-brook-115218.us-east-2.aws.neon.tech/neondb"
+  }
 }
 
 module.exports = nextConfig
